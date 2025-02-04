e858176ac607ff8e24ccb6e02ae92ff5f11296d9
Alex <alexparlaloglou@gmail.com>
2025-02-04 04:41:36 +0200

    'wait' variable defined as class attribute in 'autocomplete_page' file.

--- pages/autocomplete_page.py
+++ pages/autocomplete_page.py
@@ -8,12 +8,12 @@ class AutocompletePage:
     def __init__(self, driver, navigation_page):
         self.driver = driver
         self.Navigation_Page = navigation_page
+        self.wait = WebDriverWait(self.driver, 10)
         self.Address = (By.ID, "autocomplete")
 
 
     def send_keys_to_address(self):
         self.Navigation_Page.go_to_autocomplete()
-        wait = WebDriverWait(self.driver, 10)
-        element = wait.until(EC.presence_of_element_located(self.Address))
+        element = self.wait.until(EC.presence_of_element_located(self.Address))
         element.send_keys("Mountain View")
         return element.get_attribute("value")
