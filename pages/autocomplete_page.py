60244117032400d2e372c2ffb1038bec9b9bdc78
Alex <alexparlaloglou@gmail.com>
2025-02-04 04:47:44 +0200

    'Street Address' element added in 'autocomplete_page' and 'test_autocomplete' files.

--- pages/autocomplete_page.py
+++ pages/autocomplete_page.py
@@ -10,10 +10,17 @@ class AutocompletePage:
         self.Navigation_Page = navigation_page
         self.wait = WebDriverWait(self.driver, 10)
         self.Address = (By.ID, "autocomplete")
+        self.Street_Address = (By.ID, "street_number")
 
 
     def send_keys_to_address(self):
         self.Navigation_Page.go_to_autocomplete()
         element = self.wait.until(EC.presence_of_element_located(self.Address))
         element.send_keys("Mountain View")
+        return element.get_attribute("value")
+
+    def send_keys_to_street_address(self):
+        self.Navigation_Page.go_to_autocomplete()
+        element = self.wait.until(EC.presence_of_element_located(self.Street_Address))
+        element.send_keys("Mountain View 23")
         return element.get_attribute("value")
--- tests/test_autocomplete.py
+++ tests/test_autocomplete.py
@@ -2,3 +2,7 @@ class TestAutocomplete:
     def test_send_keys_to_address(self, autocomplete_page):
         testing_element = autocomplete_page.send_keys_to_address()
         assert testing_element == "Mountain View"
+
+    def test_send_keys_to_street_address(self, autocomplete_page):
+        testing_element = autocomplete_page.send_keys_to_street_address()
+        assert testing_element == "Mountain View 23"
